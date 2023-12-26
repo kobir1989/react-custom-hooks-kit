32f500d493b1186363ba2af1d37e835608eacbea
kobir1989 <kobir.h.ritu@gmail.com>
2023-12-26 23:12:00 +0600

    feat: add useMediaQuery hook

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import useFetch from './hooks/useFetch'
 import useForm from './hooks/useForm'
 import useLocalStorage from './hooks/useLocalStorage'
+import useMediaQuery from './hooks/useMediaQuery'
 
-export { useFetch, useForm, useLocalStorage }
+export { useFetch, useForm, useLocalStorage, useMediaQuery }
